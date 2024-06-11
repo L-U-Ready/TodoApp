e05e18b18bb20259218d5cb889953a45bb197cb7
LeeYuJoon <leeyujoon97@gmail.com>
2024-06-11 15:31:20 +0900

    front element commit

---
+++ src/main/java/org/example/todoapp/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package org.example.todoapp.repository;
+
+
+import org.example.todoapp.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+}
